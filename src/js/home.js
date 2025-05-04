9be624397b2fc8ab387486e3848c469a9b054433
Hayden Bradfield <me@haydenbradfield.com>
2025-05-04 16:05:17 -0400

    modify testimonials

--- src/js/home.js
+++ src/js/home.js
@@ -3,12 +3,21 @@ const statsSection = document.querySelector('.stats');
 const savedFigureCountup = new countUp.CountUp('saved-figure', 1000);
 const helpedClientFigureCountup = new countUp.CountUp('helped-clients-figure', 100);
 
+const nancyTestimonialMoreBtn = document.querySelector('.nancy-testimonial-more-btn');
+const nancyTestimonialExpansion = document.querySelector('.nancy-testimonial-expansion');
+
 new Swiper('.swiper', {
     loop: true,
-    autoplay: true,
-    slidesPerView: 1,
-    centeredSlides: true,
-    autoHeight: true
+    autoplay: {
+        disableOnInteraction: true,
+        pauseOnMouseEnter: true
+    },
+    slidesPerView: 1
+});
+
+nancyTestimonialMoreBtn.addEventListener('click', () => {
+    nancyTestimonialExpansion.style.display = 'block';
+    nancyTestimonialMoreBtn.style.display = 'none';
 });
 
 const observer = new IntersectionObserver((entries) => {
